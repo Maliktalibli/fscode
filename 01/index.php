6ded3f8f79a9ea34a47a825e08880e06168562ef
Maliktalibli <50819178+Maliktalibli@users.noreply.github.com>
2022-02-18 17:33:38 +0400

    task 01 edit

--- 01/index.php
+++ 01/index.php
@@ -1,23 +1,34 @@
 <?php
 
 	if (isset($_POST['misal'])) {
+
 		$misal = $_POST['misal'];
+
+	}else{
+
+		$misal = '25+54';
 	}
 	
 	 function hesabla($misal){
+
 		if(preg_match('/(\d+)([\+\-\*\/])(\d+)/', $misal, $matches) !== FALSE){
+
 			    $operator = $matches[2];
 
 			    switch($operator){
+
 			        case '+':
 			            $cavab = $matches[1] + $matches[3];
 			            break;
+
 			        case '-':
 			            $cavab = $matches[1] - $matches[3];
 			            break;
+
 			        case '*':
 			            $cavab = $matches[1] * $matches[3];
 			            break;
+			            
 			        case '/':
 			            $cavab = $matches[1] / $matches[3];
 			            break;
--- 02/index.php
+++ 02/index.php
@@ -1,73 +1,98 @@
 <?php
 
 	if (isset($_POST['misal'])) {
+
 		$misal = $_POST['misal'];
+
+	}else{
+
+		$misal = '25+54-15';
 	}
 	
 	 function hesabla($misal){
+
 		if(preg_match('/(\d+)([\+\-\*\/])(\d+)([\+\-\*\/])(\d+)/', $misal, $matches) !== FALSE){
+
 			    $operator1 = $matches[2];
 			    $operator2 = $matches[4];
 
 			    if ($operator1 == '+') {
+
 			    	switch($operator2){
+
 				        case '+':
 				            $cavab = $matches[1] + $matches[3] + $matches[5];
 				            break;
+
 				        case '-':
 				            $cavab = $matches[1] + $matches[3] - $matches[5];
 				            break;
+
 				        case '*':
 				            $cavab = $matches[1] + $matches[3] * $matches[5];
 				            break;
+
 				        case '/':
 				            $cavab = $matches[1] + $matches[3] / $matches[5];
 				            break;
 				    }
 
 			    }elseif ($operator1 == '-') {
+
 			    	switch($operator2){
+
 				        case '+':
 				            $cavab = $matches[1] - $matches[3] + $matches[5];
 				            break;
+
 				        case '-':
 				            $cavab = $matches[1] - $matches[3] - $matches[5];
 				            break;
+
 				        case '*':
 				            $cavab = $matches[1] - $matches[3] * $matches[5];
 				            break;
+
 				        case '/':
 				            $cavab = $matches[1] - $matches[3] / $matches[5];
 				            break;
 				    }
 
 			    }elseif ($operator1 == '*') {
+
 			    	switch($operator2){
 				        case '+':
 				            $cavab = $matches[1] * $matches[3] + $matches[5];
 				            break;
+
 				        case '-':
 				            $cavab = $matches[1] * $matches[3] - $matches[5];
 				            break;
+
 				        case '*':
 				            $cavab = $matches[1] * $matches[3] * $matches[5];
 				            break;
+
 				        case '/':
 				            $cavab = $matches[1] * $matches[3] / $matches[5];
 				            break;
 				    }
 
 			    }elseif ($operator1 == '/') {
+
 			    	switch($operator2){
 				        case '+':
 				            $cavab = $matches[1] / $matches[3] + $matches[5];
 				            break;
+
 				        case '-':
 				            $cavab = $matches[1] / $matches[3] - $matches[5];
 				            break;
+
 				        case '*':
 				            $cavab = $matches[1] / $matches[3] * $matches[5];
 				            break;
+				            
 				        case '/':
 				            $cavab = $matches[1] / $matches[3] / $matches[5];
 				            break;
